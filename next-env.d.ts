279bbd92f396563a3d579d91bd293d29e3959c34
NearTecmx <itimbre.achavez@gmail.com>
2026-04-16 11:40:16 -0700

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// Este archivo no debe editarse manualmente.
+// Ver: https://nextjs.org/docs/basic-features/typescript
