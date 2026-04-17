486706d48c522cbf68f582c6496ce9396a1fe53d
NearTecmx <itimbre.achavez@gmail.com>
2026-04-16 18:00:02 -0700

    Refactor next.config.js for headers and redirects

--- next.config.js
+++ next.config.js
@@ -4,51 +4,53 @@ const nextConfig = {
   poweredByHeader: false,
   compress: true,
   generateEtags: true,
-  
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-    ]
-  },
+  swcMinify: true,
+
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN',
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin',
+        },
+      ],
+    },
+  ],
+
+  redirects: async () => [
+    {
+      source: '/cotizar',
+      destination: '/#selector',
+      permanent: false,
+    },
+    {
+      source: '/contacto',
+      destination: '/#contacto',
+      permanent: false,
+    },
+  ],
 
   images: {
-    unoptimized: false,
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
+    domains: ['localhost', 'neartec.com', 'itimbre.com'],
   },
 
-  redirects: async () => {
-    return [
-      {
-        source: '/cotizar',
-        destination: '/#cotizador',
-        permanent: false,
-      },
-      {
-        source: '/contacto',
-        destination: '/#contacto',
-        permanent: false,
-      },
-    ]
+  webpack: (config) => {
+    config.optimization.minimize = true
+    return config
   },
 }
 
