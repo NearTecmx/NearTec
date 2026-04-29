dc0940ed840c4b7f8d1dfe83a4dd56531f905f70
NearTecmx <itimbre.achavez@gmail.com>
2026-04-29 03:21:19 -0700

    Crear components/TechBackground.tsx

---
+++ components/TechBackground.tsx
@@ -0,0 +1,20 @@
+export default function TechBackground() {
+  return (
+    <div className="cinema-bg" aria-hidden="true">
+      <div className="cinema-bg__wash" />
+      <div className="cinema-bg__hex cinema-bg__hex--left" />
+      <div className="cinema-bg__hex cinema-bg__hex--right" />
+      <div className="cinema-bg__grid cinema-bg__grid--top" />
+      <div className="cinema-bg__grid cinema-bg__grid--bottom" />
+      <div className="cinema-bg__circuit cinema-bg__circuit--one" />
+      <div className="cinema-bg__circuit cinema-bg__circuit--two" />
+      <div className="cinema-bg__particles cinema-bg__particles--near" />
+      <div className="cinema-bg__particles cinema-bg__particles--far" />
+      <div className="cinema-bg__beam cinema-bg__beam--one" />
+      <div className="cinema-bg__beam cinema-bg__beam--two" />
+      <div className="cinema-bg__orb cinema-bg__orb--one" />
+      <div className="cinema-bg__orb cinema-bg__orb--two" />
+      <div className="cinema-bg__vignette" />
+    </div>
+  )
+}
